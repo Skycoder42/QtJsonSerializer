9f977bd9bc1f9f20818f63dfc78d9ee150914b20
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-04-03 19:53:12 +0200

    repogen update

--- repogen.sh
+++ repogen.sh
@@ -4,4 +4,4 @@
 # $2 version
 
 myDir=$(dirname "$0")
-"$myDir/../QtModules/repogen.py" "$1" JsonSerializer "A library to perform generic seralization and deserialization of QObjects." "$2" "$myDir/LICENSE" BSD-3-Clause
+"$myDir/../QtModules/repogen.py" "$1" JsonSerializer "" "A library to perform generic seralization and deserialization of QObjects." "$2" "$myDir/LICENSE" BSD-3-Clause
